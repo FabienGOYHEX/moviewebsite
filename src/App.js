317faba8c08284dd87e3ce9284f92b7e07f637fe
Vanessa Salvador <salvadoribanez.vanessa@gmail.com>
2019-04-04 17:21:01 +0200

    Created the siteContainer (general container of the site) in app.js, which for the moment has a black backgroundColor, that later we will change for a backgroundImage. In addition, created the tag Container, for the general left and right margin of the site.

--- src/App.js
+++ src/App.js
@@ -3,16 +3,24 @@ import logo from './logo.svg';
 import './App.css';
 import PrimaryNavBar from './PrimaryNavbar'
 import NewsCarousel from './NewsCarousel'
+import {
+  Container,
+  Row,
+  Col} from 'reactstrap';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <PrimaryNavBar />
-
-        <NewsCarousel />
-        
-      </div>
+      <div className="PrimaryContainer">
+      <Container>
+      <Row>  
+      <Col><PrimaryNavBar /></Col>
+      </Row>
+      <Row>  
+      <Col><NewsCarousel /></Col>
+      </Row> 
+        </Container>x
+        </div>
     );
   }
 }
