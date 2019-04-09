94cb19c992651418a96d2b32325db17793b0d35d
fabiengoyhex <fabien.goyhex@gmail.com>
2019-04-09 10:35:16 +0200

    created footer elements

--- src/Footer.js
+++ src/Footer.js
@@ -2,15 +2,36 @@
 Child component of App.js which contains the social Networks, the top button and the navbar items
  */
 import React, { Component } from 'react';
+import  "./App.css"
+import {Row,Col} from 'reactstrap'
 
 class Footer extends Component {
 
     render() {
         return (
-            <footer style={{backgroundColor:'pink'}}>
+            <footer >
+            <Row>
+                <Col>
                 <img src="https://via.placeholder.com/150" />
-                <i  class="fab fa-facebook-square fa-5x">youhou</i>
-                
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+               <a href="http://facebook.com" target = "_blank"> <i  class="fab fa-facebook-square fa-3x"></i></a>
+               <a href="http://twitter.com" target = "_blank"> <i class="fab fa-twitter-square fa-3x"></i></a>
+               <a href="http://github.com" target = "_blank"> <i class="fab fa-github-square fa-3x"></i></a>
+               <a href="http://pinterest.com" target = "_blank"> <i class="fab fa-pinterest-square fa-3x"></i></a>
+               </Col>               
+            </Row>
+            <Row>
+                <Col>
+               <a href="#">Movies </a>
+               <a href="#">Series </a>
+               </Col>
+            </Row>
+            
+               <a href="#"> <i class="fas fa-arrow-alt-circle-up fa-3x"></i></a>
+             
 
             </footer> 
         );
